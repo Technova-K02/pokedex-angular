14cdbcf485795412336dc5c38921fb5062546456
Technova-K02 <michaelsmith1995711@gmail.com>
2026-05-19 17:04:04 +0200

    chore: add local graphql server and apollo named clients

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideHttpClient } from "@angular/common/http";
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { provideAnimations } from "@angular/platform-browser/animations";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
+import { provideGraphqlClients } from "./graphql/apollo.providers";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    provideGraphqlClients(),
+  ],
 };
---
+++ src/app/graphql/graphql-endpoints.ts
@@ -0,0 +1,8 @@
+/**
+ * GraphQL endpoint constants used across the app.
+ */
+export const GRAPHQL_ENDPOINTS = {
+  pokeapi: "https://beta.pokeapi.co/graphql/v1beta",
+  local: "http://localhost:4000/graphql",
+} as const;
+
